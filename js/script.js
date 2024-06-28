aaef34db14bddbf78b19eefbe9eb17bc12c5235b
ceccobell <francescobell2001@gmail.com>
2024-06-28 13:17:59 +0200

    [UPD] defined email array

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-alert("TEST");
+const list_email = [
+  "mario.rossi@example.com",
+  "anna.bianchi@example.com",
+  "luca.verdi@example.com",
+  "francesca.neri@example.com",
+  "giulia.ferrari@example.com",
+  "alessandro.martini@example.com",
+  "sara.conti@example.com",
+  "marco.rinaldi@example.com",
+  "laura.galli@example.com",
+  "andrea.moro@example.com",
+];
